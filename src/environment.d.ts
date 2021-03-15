223005e251ab74a5081fe35b50affcae4324a676
Antoine Muller <niantoinesan@gmail.com>
2021-03-15 09:12:10 +0100

    add build:production

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,9 +2,9 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       REACT_APP_MEMODOGS_API_URL: string;
-      NODE_ENV: 'development' | 'production';
+      NODE_ENV: "development" | "production";
     }
   }
 }
 
-export {}
+export {};
